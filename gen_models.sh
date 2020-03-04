3e34663ee0497244e1ccb85a25996b2c30e470bd
scnjl <scnjl@qq.com>
2020-03-04 16:08:42 +0800

    fix gen_models

--- gen_models.sh
+++ gen_models.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sqlacodegen --outfile=./application/models.py mysql+pymysql://username:password@localhost:3306/database?charset=utf8mb4
-sqlacodegen --flask --outfile=./application/flask/models.py mysql+pymysql://username:password@localhost:3306/database?charset=utf8mb4
+sqlacodegen --flask --outfile=./application/flask_models.py mysql+pymysql://username:password@localhost:3306/database?charset=utf8mb4
