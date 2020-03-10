ee0992edc77e721806f5723bb98ac97655ddc086
scnjl <scnjl@qq.com>
2020-03-10 16:32:44 +0800

    fix gen_make

--- gen_models.sh
+++ gen_models.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sqlacodegen --outfile=./application/models.py mysql+pymysql://username:password@localhost:3306/database?charset=utf8mb4
-flask-sqlacodegen --outfile=./application/flask_models.py mysql+pymysql://username:password@localhost:3306/database?charset=utf8mb4
+sqlacodegen --outfile=./application/models/__init__.py mysql+pymysql://username:password@localhost:3306/database?charset=utf8mb4
+flask-sqlacodegen --outfile=./application/models/flask.py mysql+pymysql://username:password@localhost:3306/database?charset=utf8mb4
