e12c4590497d55a4709d4d92451f274d943121ba
scnjl <scnjl@qq.com>
2020-04-20 00:07:09 +0800

    upgrade utils4py

--- instance/app.py
+++ instance/app.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
+
+import os
+
+APP_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+APP_URL = os.getenv("APP_URL", "http://127.0.0.1:8000")
+
+UPLOAD_FOLDER = APP_ROOT + "/application/public"
