eb2f3e5737ca2bb3abd5ff57cbeef0fc87779e78
zhuchenglin <scnjl@qq.com>
2021-04-06 17:51:01 +0800

    add debug in vscode

--- run.py
+++ run.py
@@ -3,13 +3,20 @@
 
 import logging
 import application
-
+import sys
 
 app = application.create_app()
 
-if __name__ == "__main__":
-    app.run(host=app.config["HOST"], port=app.config["PORT"], debug=app.config["DEBUG"])
-    app.logger.setLevel(logging.getLevelName(app.config["LOG_LEVEL"].upper()))
+
+if "run" in sys.argv:
+    # debug in vscode, "run" in sys.argv
+    # and run.py
+    log_level = logging.getLevelName(app.config["LOG_LEVEL"].upper())
+    app.logger.setLevel(log_level)
+    if __name__ == "__main__":
+        app.run(
+            host=app.config["HOST"], port=app.config["PORT"], debug=app.config["DEBUG"]
+        )
 else:
     gunicorn_logger = logging.getLogger("gunicorn.error")
     app.logger.handlers = gunicorn_logger.handlers
